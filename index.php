e414130f21c46d5fb68da606d4d95de0a6d8ab83
Muhammad Ali <softeng.aliijaz@gmail.com>
2024-10-19 03:10:15 +0500

    first program after setup

--- index.php
+++ index.php
@@ -1,13 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Learning</title>
+    <style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
+
+    .action-button {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+    .button {
+        background-color: blue;
+        height: 60px;
+        width: 350px;
+        border: 10;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+        color: white;
+        font-weight: bold;
+        font-style: italic;
+    }
+
+    .anchor {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 60px;
+        width: 350px;
+        background-color: blue;
+        padding: 20px;
+        margin: 0 auto;
+        border-radius: 10px;
+    }
+
+    .anchor a {
+        color: white;
+        text-decoration: none;
+        font-size: 20px;
+        font-weight: bold;
+        font-style: italic;
+    }
+    </style>
 </head>
+
 <body>
-    <h1>
-        <?php echo 'Hello World'; ?>
-    </h1>
+    <div class="anchor">
+        <a target="_blank" href="https://www.linkedin.com/in/muhammad-ali-ijaz/">Press Me to go to LinkedIn</a>
+    </div>
+    <div class="action-button">
+        <button class="button">
+            Press Me
+        </button>
+    </div>
 </body>
+
 </html>
