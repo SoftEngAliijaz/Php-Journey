ff6bf977615eb6bcdc430c452fd6373714110051
Muhammad Ali <softeng.aliijaz@gmail.com>
2024-10-20 03:06:42 +0500

    form handling with css

--- backend/php_forms/form_handling.php
+++ backend/php_forms/form_handling.php
@@ -4,28 +4,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Handling in Php</title>
+    <title>PHP Form Handling</title>
+    <link rel="stylesheet" type="text/css" href="form_handling_styles.css">
 </head>
 
 <body>
-    <!-- POST -->
-    <form action="welcome.php" method="POST">
-        Name: <input type="text" name="Name">
-        Email: <input type="text" name="Email">
-        <input type="submit">
-    </form>
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email address is: <?php echo $_POST["email"]; ?>
+    <div class="form-container">
+        <form class="form-decoration" action="form_handling.php" method="post">
+            <label for="name">Enter Your Name:</label>
+            <input type="text" id="name" name="name" required>
 
+            <label for="email">Enter Your Email:</label>
+            <input type="email" id="email" name="email" required>
 
-    <!-- GET -->
-    <form action="welcome_get.php" method="GET">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
-    </form>
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email address is: <?php echo $_POST["email"]; ?>
+            <label for="phone">Enter Your Phone Number:</label>
+            <input type="tel" id="phone" name="phone" required>
+
+            <label for="comment">Enter Your Comment:</label>
+            <input type="text" id="comment" name="comment" required>
+
+            <input type="submit" id="submit-button" value="Submit">
+        </form>
+    </div>
+
+    <?php
+    $name = $email = $phone = $comment = "";
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = test_input($_POST['name']);
+        $email = test_input($_POST['email']);
+        $phone = test_input($_POST['phone']);
+        $comment = test_input($_POST['comment']);
+
+        echo "<h3>Your Input:</h3>";
+        echo "Name: $name<br>";
+        echo "Email: $email<br>";
+        echo "Phone: $phone<br>";
+        echo "Comment: $comment<br>";
+    }
+    ?>
 </body>
 
 </html>
