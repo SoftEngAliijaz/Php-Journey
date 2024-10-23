413876eee0759b8e85e84cd1fb0540a4b13aa84d
Muhammad Hassaan <cs.hassaan@gmail.com>
2024-10-24 02:29:05 +0500

    DevX login and sign up screen

--- php/auth/login.php
+++ php/auth/login.php
@@ -1,7 +1,41 @@
-<?php
-echo "This is Php Login <br><br>";
-
-if ($_GET) {
-    echo "User Name is " . $_GET['name'] . "<br><br>";
-    echo "User Password is " . $_GET['password'];
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp Page</title>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+</head>
+<body>
+   <div class="main">
+       <div class="space">
+           <h3>Welcome to Php Journey</h3>
+         
+      
+       </div>
+       <div class="signUp">
+           <div>
+               <h2>Log In</h2>
+           </div>
+           <div class="fields">
+               <form action="" method="post">
+                  
+                   <div class="input-field">
+                       <input class="input" type="email" name="email" placeholder="E-mail">
+                   </div>
+                   <div class="input-field">
+                       <input class="input" type="password" name="password" placeholder="Enter Password">
+                   </div>
+                  
+                   <div class="box">
+                    <input type="checkbox" ><span>by logining in  you accept terms & services </span>
+                   </div>
+                   <div>
+                       <button type="submit" class="btn" action="sign_up.php">Log In</button>
+                   </div>
+               </form>   
+           </div>   
+       </div>
+   </div>
+</body>
+</html>
--- php/auth/sign_up.php
+++ php/auth/sign_up.php
@@ -1,8 +1,46 @@
-<?php
-echo "<h1>This is Php Login</h1> <br><br>";
-
-if ($_POST) {
-    echo "Your User Name is:" . $_POST['username'];
-    echo "Your Email is:" . $_POST['email'];
-    echo "Your Password is:" . $_POST['password'];
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp Page</title>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+</head>
+<body>
+   <div class="main">
+       <div class="space">
+           <h3>Welcome to Php Journey</h3>
+         
+      
+       </div>
+       <div class="signUp">
+           <div>
+               <h2>Sign Up</h2>
+           </div>
+           <div class="fields">
+               <form action="" method="post">
+                   <div class="name-fields">
+                       <input class="mid" type="text" name="fName" placeholder="First Name">
+                       <input class="mid" type="text" name="lName" placeholder="Last Name">
+                   </div>
+                   <div class="input-field">
+                       <input class="input" type="email" name="email" placeholder="E-mail">
+                   </div>
+                   <div class="input-field">
+                       <input class="input" type="password" name="password" placeholder="Enter Password">
+                   </div>
+                   <div class="input-field">
+                       <input class="input" type="password" name="confirmPass" placeholder="Confirm Password">
+                   </div>
+                   <div class="box">
+                    <input type="checkbox" ><span>by signing up you accept terms & services </span>
+                   </div>
+                   <div>
+                       <button type="submit" class="btn">Sign Up</button>
+                   </div>
+               </form>   
+           </div>   
+       </div>
+   </div>
+</body>
+</html>
