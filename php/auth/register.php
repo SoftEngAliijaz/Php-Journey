1ef258cfc60afa8ec9e35dfae91895a7f355cc90
Muhammad Ali <softeng.aliijaz@gmail.com>
2024-10-24 03:03:54 +0500

    responsive login signup ui

--- php/auth/register.php
+++ php/auth/register.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php Journey Login</title>
+    <title>Php Journey SignUp</title>
     <link rel="stylesheet" href="styles.css" type="text/css">
 </head>
 
@@ -13,26 +13,32 @@
         <div class="space">
             <h2 class="logintitle">Welcome to Php Journey</h2>
         </div>
-        <div class="login-Box">
+
+        <div class="signUp-Box">
             <div>
-                <h2>Log In</h2>
+                <h2>Sign Up</h2>
             </div>
             <div class="fields">
                 <form action="" method="post">
+                    <div class="name-fields">
+                        <input class="mid" type="text" name="fName" placeholder="First Name">
+                        <input class="mid" type="text" name="lName" placeholder="Last Name">
+                    </div>
                     <div class="input-field">
                         <input class="input" type="email" name="email" placeholder="E-mail">
                     </div>
                     <div class="input-field">
                         <input class="input" type="password" name="password" placeholder="Enter Password">
                     </div>
-
+                    <div class="input-field">
+                        <input class="input" type="password" name="confirmPass" placeholder="Confirm Password">
+                    </div>
                     <div class="checkbox">
                         <label for="checkbox"><input type="checkbox"></label><span> I agree to the terms and
                             conditions</span>
                     </div>
-
                     <div>
-                        <button type="submit" class="btn" action="sign_up.php">Log In</button>
+                        <button type="submit" class="btn">Sign Up</button>
                     </div>
                 </form>
             </div>
